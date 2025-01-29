79f3f98dc8f67f2232aac6001ba16d8e0dabbdbe
Aslam youseph <aslamyouseph@gmail.com>
2025-01-29 19:24:11 +0530

    Reactjs and Nodejs connection created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; //This us used to connect the frontend to the backend
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from Node.js server
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        setData(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Hello its Aslam youseph</h1>
+      <h1>data: {data}</h1>
     </div>
   );
 }
