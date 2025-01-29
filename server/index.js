163d2047922f1b04b632e6c7d6f58fc8f758a135
Aslam youseph <aslamyouseph@gmail.com>
2025-01-29 20:20:27 +0530

    connection is esstablished

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+var createError = require("http-errors");
 const cors = require("cors");
+var path = require("path");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session"); //it is used for handing the session operations
+const connectDB = require("./DB-configiration/DB-connection"); // import the connection file
+
+// calling the connection function to connect to the database
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000; // This project is run on the port number 5000
